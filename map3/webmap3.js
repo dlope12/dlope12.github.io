1af888071f891ac855eed42eec63aa4543c63b9b
dlope12 <48488109+dlope12@users.noreply.github.com>
2019-04-16 20:51:19 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -3,19 +3,21 @@ L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{
 let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   let stateStyle = function (feature) {
-  let age = feature.properties.MED_AGE // get the current state's Median Age attribute
-  let stateColor = 'olive' // let the initial color be a darker green
-  if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
+  let age = feature.properties.AVE_HH_SZ // get the current state's Median Age attribute
+  let stateColor = 'purple' // let the initial color be a darker green
+  if ( age > 2.5 ) { stateColor = 'blue' } // if the state's median age is less than the average, color it a lighter green
   return {
     color: stateColor, //use the color variable above for the value
     weight: 1,
-    fillOpacity: 0.2
+    fillOpacity: 0.2,
+    opacity: 1.0
+
   }
 }
 let onEachFeature = function (feature, layer) {
   let name = feature.properties.STATE_NAME
-       let age = feature.properties.MED_AGE
-       layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+       let age = feature.properties.AVE_HH_SZ
+       layer.bindPopup('Median House Hold Size of ' + name + ': ' + age + '<br>National average: 2.5')
    }
   let stateGeojsonOptions = {
     style: stateStyle,
